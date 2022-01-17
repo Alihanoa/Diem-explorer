a61c172bfa09395c83968fdd50ea389eb94b781b
Tim Niestrath <Tim.Niestrath@studmail.w-hs.de>
2022-01-17 20:49:24 +0100

    added loggingfunction into file and expanded rest api and repos

---
+++ explorer/src/test/java/com/diemexplorer/explorer/TransactionControllerTest.java
@@ -0,0 +1,33 @@
+//package com.diemexplorer.explorer;
+//import com.diemexplorer.explorer.Controller.TransactionController;
+//import com.diemexplorer.explorer.Entities.Transactions;
+//import com.diemexplorer.explorer.Repositories.TransactiondetailsRepository;
+//import com.diemexplorer.explorer.Repositories.TransactionsRepository;
+//import java.util.List;
+//import java.util.Optional;
+//import static org.junit.Assert.*;
+//import org.junit.Before;
+////import org.junit.Test;
+//import org.junit.jupiter.api.Test;
+//import org.junit.jupiter.api.Assertions.*;
+////import org.junit.jupiter.api.
+//public class TransactionControllerTest {
+//    TransactionsRepository tr;
+//    TransactiondetailsRepository tdr;
+//    TransactionController tc;
+//    
+////    @Before
+////    public void setUp(){
+////        
+////    
+////   
+////    }
+//    
+//    @Test
+//    public void t1(){
+//        
+//        tc = new TransactionController(tr  , tdr);
+//        assertEquals(tc.getNumberOfTransactionsToday()>5, true);
+//    
+//    }
+//}
